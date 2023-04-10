119b6c9b5b28cf4782f457b463d47f9bcf6ffead
无言独上机房 <88866917+sjmshsh@users.noreply.github.com>
2023-04-10 10:01:01 +0800

    Create GunFactory.go

---
+++ 工厂模式/GunFactory.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// 枪支工厂
+func GetGun(gunType string) (IGun, error) {
+	if gunType == "ak47" {
+		return NewAk47(), nil
+	}
+	if gunType == "musket" {
+		return NewMusket(), nil
+	}
+	return nil, fmt.Errorf("wrong gun type passed")
+}
